10be74ee9d1fd97d8e497227301d7b85b3687aab
kavv210 <kaavyagera2101@gmail.com>
2025-06-29 15:33:30 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
