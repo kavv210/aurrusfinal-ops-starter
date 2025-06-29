04964e8535e2139abb820f52528daa16a75ccbcc
kavv210 <kaavyagera2101@gmail.com>
2025-06-30 03:06:06 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,9 +6,20 @@ import '../css/main.css';
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
-      duration: 800,
-      once: true
+      duration: 800,     // Animation duration
+      once: true,        // Animate only once per element
+      easing: 'ease-in-out',
     });
+
+    // Optional: refresh on route change (for dynamic content)
+    const handleRouteChange = () => {
+      AOS.refresh();
+    };
+
+    // Clean up on unmount
+    return () => {
+      window.removeEventListener('hashchange', handleRouteChange);
+    };
   }, []);
 
   return <Component {...pageProps} />;
