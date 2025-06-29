de4d6fdaac898ba13b85645aec6b33c5810a4a31
kavv210 <kaavyagera2101@gmail.com>
2025-06-30 02:39:25 +0530

    Create HeroSection.tsx

---
+++ src/components/sections/HeroSection.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classnames';
+import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
+import { TitleBlock } from '../blocks/TitleBlock';
+
+export default function HeroSection(props) {
+  const { title, subtitle, colors = 'bg-light-fg-dark', styles = {}, elementId } = props;
+
+  return (
+    <section
+      id={elementId}
+      className={classNames(
+        'sb-hero-section',
+        colors,
+        styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined
+      )}
+    >
+      <div className="mx-auto max-w-6xl px-4 text-center">
+        {title && <TitleBlock {...title} />}
+        {subtitle && <p className="text-xl mt-4">{subtitle}</p>}
+      </div>
+    </section>
+  );
+}
