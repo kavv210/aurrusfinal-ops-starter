c3438419a161e767f2a0ce4f62c57fffd4404c2a
kavv210 <kaavyagera2101@gmail.com>
2025-07-05 01:47:52 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,55 +1,45 @@
-// pages/_app.js
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '../css/main.css';
+import dynamic from 'next/dynamic';
+
+const ScrollToTop = dynamic(() => import('../components/ScrollToTop'), { ssr: false });
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  // AOS animations
+  /* AOS */
   useEffect(() => {
-    AOS.init({
-      duration: 800,
-      offset: 80,
-      easing: 'ease-in',
-      once: false,
-      mirror: true,
-      anchorPlacement: 'top-bottom',
-      throttleDelay: 99,
-      debounceDelay: 50,
-    });
-
+    AOS.init({ duration: 800, offset: 80, once: false, mirror: true });
     const refresh = () => AOS.refreshHard();
     router.events.on('routeChangeComplete', refresh);
     return () => router.events.off('routeChangeComplete', refresh);
   }, [router.events]);
 
-  // Lenis smooth scroll
+  /* Lenis */
   useEffect(() => {
     (async () => {
       const { default: Lenis } = await import('@studio-freight/lenis');
-
       const lenis = new Lenis({
         duration: 1.2,
         easing: t => 1 - Math.pow(1 - t, 3),
         smoothWheel: true,
         smoothTouch: false,
-        wheelMultiplier: 1,
-        touchMultiplier: 2
       });
-
-      function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
-
+      function raf(time) { lenis.raf(time); requestAnimationFrame(raf); }
       requestAnimationFrame(raf);
       document.documentElement.classList.add('lenis');
+      document.body.classList.add('lenis');
       console.log('Lenis success');
     })();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ScrollToTop />
+    </>
+  );
 }
