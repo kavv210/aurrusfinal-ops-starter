fab7dc385c1bdafd420eba126ec74e73d69a738a
kavv210 <kaavyagera2101@gmail.com>
2025-07-05 00:53:32 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,26 @@
+// pages/_app.js
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import '../css/main.css';
+import '../css/main.css';   // Tailwind output
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
+    // Initialise once on mount
     AOS.init({
-      duration: 700,
-      once: true,
-      offset: 120,
+      duration: 2000,      // each fade lasts 2 s
+      delay: 100,          // small global delay
+      offset: 160,         // start only when 160 px inside viewport
       easing: 'ease-in-out',
+      once: true,          // animate once per element
+      mirror: true         // replay when scrolling back up
     });
 
+    // Refresh on route change
     const handleRouteChange = () => AOS.refresh();
-
     router.events.on('routeChangeComplete', handleRouteChange);
 
     return () => {
