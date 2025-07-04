8ed54c5424b402d1bbdb9025d0eb11d6009db566
kavv210 <kaavyagera2101@gmail.com>
2025-07-04 12:49:27 +0530

    Update index.tsx

--- src/components/blocks/TitleBlock/index.tsx
+++ src/components/blocks/TitleBlock/index.tsx
@@ -1,31 +1,32 @@
 import * as React from 'react';
 import classNames from 'classnames';
-
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
 
-export default function TitleBlock(props) {
-    const {
-        className, // this is picked from props.title.className
-        text = '',
-        color = 'text-dark',
-        styles = {},
-        level = 2 // optional: allow setting h1/h2/etc
-    } = props;
+export default function TitleBlock({
+  className = '',
+  text = '',
+  color = 'text-dark',
+  styles = {},
+  level = 2,
+  ...rest
+}) {
+  const HeadingTag = `h${level}` as any;
+  const fieldPath = rest['data-sb-field-path'];
 
-    const HeadingTag = `h${level}` as any;
-    const fieldPath = props['data-sb-field-path'];
-
-    return (
-        <HeadingTag
-            className={classNames(
-                'sb-component sb-component-block sb-component-title',
-                color,
-                mapStyles(styles?.self ?? {}),
-                className
-            )}
-            data-sb-field-path={fieldPath}
-        >
-            <span {...(fieldPath && { 'data-sb-field-path': '.text' })}>{text}</span>
-        </HeadingTag>
-    );
+  return (
+    <HeadingTag
+      className={classNames(
+        'sb-component sb-component-block sb-component-title',
+        // ↓ force bold, tracking, hover zoom
+        'font-extrabold tracking-tight',
+        'transition-transform duration-300 hover:scale-105',
+        color,
+        mapStyles(styles?.self ?? {}),
+        className
+      )}
+      data-sb-field-path={fieldPath}
+    >
+      <span {...(fieldPath && { 'data-sb-field-path': '.text' })}>{text}</span>
+    </HeadingTag>
+  );
 }
