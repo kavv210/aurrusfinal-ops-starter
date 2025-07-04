5d529e0b02df07d65767642b3166e9755ebd2331
kavv210 <kaavyagera2101@gmail.com>
2025-07-04 12:55:26 +0530

    Update index.tsx

--- src/components/blocks/TitleBlock/index.tsx
+++ src/components/blocks/TitleBlock/index.tsx
@@ -1,32 +1,34 @@
 import * as React from 'react';
 import classNames from 'classnames';
+
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
 
-export default function TitleBlock({
-  className = '',
-  text = '',
-  color = 'text-dark',
-  styles = {},
-  level = 2,
-  ...rest
-}) {
-  const HeadingTag = `h${level}` as any;
-  const fieldPath = rest['data-sb-field-path'];
+export default function TitleBlock(props) {
+    const {
+        className,
+        text = '',
+        color = 'text-dark',
+        styles,
+        level = 2
+    } = props;
 
-  return (
-    <HeadingTag
-      className={classNames(
-        'sb-component sb-component-block sb-component-title',
-        // ↓ force bold, tracking, hover zoom
-        'font-extrabold tracking-tight',
-        'transition-transform duration-300 hover:scale-105',
-        color,
-        mapStyles(styles?.self ?? {}),
-        className
-      )}
-      data-sb-field-path={fieldPath}
-    >
-      <span {...(fieldPath && { 'data-sb-field-path': '.text' })}>{text}</span>
-    </HeadingTag>
-  );
+    const HeadingTag = `h${level}` as any;
+    const fieldPath = props['data-sb-field-path'];
+
+    // Defensive check to ensure styles is an object and styles.self exists
+    const selfStyles = (styles && typeof styles === 'object' && 'self' in styles && styles.self) ? styles.self : {};
+
+    return (
+        <HeadingTag
+            className={classNames(
+                'sb-component sb-component-block sb-component-title',
+                color,
+                mapStyles(selfStyles),
+                className
+            )}
+            data-sb-field-path={fieldPath}
+        >
+            <span {...(fieldPath && { 'data-sb-field-path': '.text' })}>{text}</span>
+        </HeadingTag>
+    );
 }
