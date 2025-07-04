eaa28131b30939aa3ed9d4326372fada973693a2
kavv210 <kaavyagera2101@gmail.com>
2025-07-04 12:52:45 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import '../css/main.css';
+import '../css/main.css'; // Tailwind styles
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -10,14 +10,13 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
       duration: 800,
-      once: false,            // Animates every scroll
+      once: false,          // re-trigger animations on scroll
+      mirror: true,         // animate out on scroll-up
       easing: 'ease-out-cubic',
-      mirror: true,           // Re-animates when scrolling back
-      offset: 120,
+      offset: 120
     });
 
     const handleRouteChange = () => AOS.refresh();
-
     router.events.on('routeChangeComplete', handleRouteChange);
 
     return () => {
