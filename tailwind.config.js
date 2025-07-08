04118ee51ba4c2dde14cfe87a8ca7658e1a03a7d
kavv210 <kaavyagera2101@gmail.com>
2025-07-08 16:59:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,6 +54,17 @@ module.exports = {
   ],
 
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+       '2xl': '1536px'
+      }
+    },
     extend: {
       fontWeight: {
         normal: '400',
